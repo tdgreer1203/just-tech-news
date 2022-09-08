34fd1eb1bfce7d2937fe03c407185db4952d2760
Theo <tdgreer1203@gmail.com>
2022-09-07 21:56:47 -0400

    Creating User Routes

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        //Table Column Definitions Go Here
+        //define an id column
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        //Table Configuration Optoins Go Here
+        //Pass in Our Imported Sequelize Connection (the direct connection to our database)
+        sequelize,
+        //don't automatically create createdAt/updatedAt timestamp fields
+        timestamps: false,
+        //don't pluralize name of database table
+        freezeTableName: true,
+        //use underscores instead of camel-casing
+        underscored: true,
+        //make our model name stay lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
